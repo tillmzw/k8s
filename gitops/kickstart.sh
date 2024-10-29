bcda3ceb5017b1ade5c78174c892f67a6d3815c1
Till Meyer zu Westram <till@meyerzuwestram.net>
2024-10-29 13:55:31 +0100

    update bootstrapping to refer to the argocd configuration

--- gitops/kickstart.sh
+++ gitops/kickstart.sh
@@ -5,10 +5,11 @@ test -f ./argo-values.yaml || exit 1
 (
     helm repo add argo https://argoproj.github.io/argo-helm
 
-    helm install argocd argo/argo-cd \
+    helm install argo-cd argo/argo-cd \
         --namespace=argocd \
         --create-namespace \
         -f argo-values.yaml
 
-    kubectl apply -n argocd -f apps/root.yaml
+    kubectl apply -n argocd -f appproj-infrastructure.yaml
+    kubectl apply -n argocd -f appset-infrastructure-configs.yaml
 )
