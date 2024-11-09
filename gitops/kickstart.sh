2d13562363ba0442572778378331c80f6a424ff2
Till Meyer zu Westram <till@meyerzuwestram.net>
2024-11-09 12:34:15 +0100

    add doc

--- gitops/kickstart.sh
+++ gitops/kickstart.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-test -f ./argo-values.yaml || exit 1
+helm repo add argo https://argoproj.github.io/argo-helm
 
-(
-    helm repo add argo https://argoproj.github.io/argo-helm
+helm install argo-cd argo/argo-cd \
+	--namespace=argocd \
+	--create-namespace
 
-    helm install argo-cd argo/argo-cd \
-        --namespace=argocd \
-        --create-namespace \
-        -f argo-values.yaml
-
-    kubectl apply -n argocd -f appproj-infrastructure.yaml
-    kubectl apply -n argocd -f appset-infrastructure-configs.yaml
-)
+kubectl apply -n argocd -f infrastructure/configs/argo-cd-cfg/appproj-infrastructure.yaml
+kubectl apply -n argocd -f infrastructure/configs/argo-cd-cfg/appset-infrastructure-configs.yaml
